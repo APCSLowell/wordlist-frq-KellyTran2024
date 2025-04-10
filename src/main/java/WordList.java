5cf1ddbe7bfbfb453e5305ef60a4baf35c701fde
KellyTran2024 <ketran4@s.sfusd.edu>
2025-04-09 20:42:22 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -22,7 +22,7 @@ public class WordList
   public void removeWordsOfLength(int len)
   {
    for(int i = myList.size()-1;i>=0; i++){
-     if(myList.get(i).length == len){
+     if(myList.get(i).length() == len){
        myList.remove(i);
      }
   }
